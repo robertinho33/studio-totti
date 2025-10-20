d543ef125a7115befbce899bfe0e5c88ea9286a7
robertinho33 <sou_roberto@hotmail.com>
2025-10-20 02:18:55 -0300

    x

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("btnAgendar");
+    botao.addEventListener("click", () => {
+      alert("Em breve você poderá agendar seu horário online!");
+    });
+  });
+  
